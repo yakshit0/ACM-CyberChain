d434609bd27104b42690284aba8bdd6854c67ca3
Yakshit <yakshitgiri06@gmail.com>
2024-10-23 00:00:38 +0530

    major change #1

--- taxing (task1.5).js
+++ taxing (task1.5).js
@@ -1,11 +1,18 @@
 const taxlist = [[1,17.5],[2,9],[3,35],[4,20]]
 const pricelist = [[1,860],[2,1500],[3,475],[4,255]]
 
-function taxincluder(type){
+let tax = 0.0;
+
+function taxcalculator(type){
     taxrate = taxlist.find(x => x.includes(type))
     price = pricelist.find(x => x.includes(type))
 
-    let total = price[1]*(1 + taxrate[1]/100)
-    console.log(`Price of item of type ${price[0]} is ${total}`);
-    
+    tax = price[1]*taxrate[1]/100
+    console.log(`Tax on item of type ${price[0]} is ${tax}`)
+}
+
+function totalprice(type){
+    taxcalculator(type);
+    let total = price[1] + tax
+    console.log(`Total price of item of type ${price[0]} is ${total}`);
 }
